75ec894a98688e27b821372f972d68517b10722b
Igor Antonov <keritaf@gmail.com>
2012-05-07 00:38:31 +0300

    Adding Presentation model entity
    Signed-off-by: Igor Antonov <keritaf@gmail.com>

---
+++ Itransition.BrowserPoint.Model/Entities/Presentation.cs
@@ -0,0 +1,31 @@
+﻿namespace Itransition.BrowserPoint.Model.Entities
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// Presentation entity.
+    /// </summary>
+    public class Presentation
+    {
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Presentation title.
+        /// </summary>
+        [Required]
+        public String Title { get; set; }
+
+        /// <summary>
+        /// Presentation description.
+        /// </summary>
+        public String Description { get; set; }
+
+        /// <summary>
+        /// Presentation owner.
+        /// This user has full control over presentation attributes and content.
+        /// </summary>
+        public User Owner { get; set; }
+    }
+}
