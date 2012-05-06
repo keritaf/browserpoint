eba96e22e948280d71e05ef7289a3405f21b0bdc
Igor Antonov <keritaf@gmail.com>
2012-05-06 17:50:57 +0300

    Replacing Twitter Bootstrap LESS version to CSS one
    Signed-off-by: Igor Antonov <keritaf@gmail.com>

--- Itransition.BrowserPoint.Web/CassetteConfiguration.cs
+++ Itransition.BrowserPoint.Web/CassetteConfiguration.cs
@@ -1,5 +1,4 @@
 using Cassette.Configuration;
-using Cassette.Scripts;
 using Cassette.Stylesheets;
 
 namespace Itransition.BrowserPoint.Web
@@ -11,26 +10,14 @@ namespace Itransition.BrowserPoint.Web
     {
         public void Configure(BundleCollection bundles, CassetteSettings settings)
         {
-            // TODO: Configure your bundles here...
-            // Please read http://getcassette.net/documentation/configuration
+            bundles.AddUrlWithLocalAssets(@"//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js",
+                new LocalAssetSettings
+                    {
+                        Path = "scripts/jquery/jquery.js",
+                        FallbackCondition = "!window.jQuery"
+                    });
 
-            // This default configuration treats each file as a separate 'bundle'.
-            // In production the content will be minified, but the files are not combined.
-            // So you probably want to tweak these defaults!
-            bundles.AddPerIndividualFile<StylesheetBundle>("Content");
-            bundles.AddPerIndividualFile<ScriptBundle>("Scripts");
-
-            // To combine files, try something like this instead:
-            //   bundles.Add<StylesheetBundle>("Content");
-            // In production mode, all of ~/Content will be combined into a single bundle.
-            
-            // If you want a bundle per folder, try this:
-            //   bundles.AddPerSubDirectory<ScriptBundle>("Scripts");
-            // Each immediate sub-directory of ~/Scripts will be combined into its own bundle.
-            // This is useful when there are lots of scripts for different areas of the website.
-
-            // *** TOP TIP: Delete all ".min.js" files now ***
-            // Cassette minifies scripts for you. So those files are never used.
+            bundles.AddPerIndividualFile<StylesheetBundle>(@"Content");
         }
     }
 }
